7f44440d6caeb75c4927280e0aed604b851c0280
bengroves <bengroves15@gmail.com>
2020-11-17 19:40:11 -0500

    commit tree f804dd91c9552200d45be721b28e40dfddba2673

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', "Brown","Cute", 1);
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Rowdy', 12, 'M', "Brown","Cute", 1);
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Tabitha', 1, 'F', "Brown","Mad", 1);
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Melissa', 10, 'F', "Brown","Cute", 1);
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Grinch', 11, 'M', "Brown","Cool", 1);
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Wendy', 12, 'M', "Brown","Cute", 1);
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Sergeant Brown', 12, 'F', "Brown","Cute", 0);
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES (NULL, 12, 'M', "Brown","Cute", 1)
